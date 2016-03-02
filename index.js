2b82dc211530c868e36ceb6d618c0521328b65ae
msalli <alli.cernoch@gmail.com>
2016-03-01 23:44:20 -0800

    modal for search, vertical positioning for elements

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+$(document).ready(function() {
 
-  var searchLink = document.getElementById('search-link');
-    searchLink.onclick = function(event) {
+  triggerModal();
+  hideModal();
+  handleOutsideClick();
 
-  };
+  function triggerModal() {
+    $('#search-link').click(function() {
+      $('.search-modal-overlay').fadeIn('fast');
+    })
+  }
 
+  function hideModal() {
+    $('.close-modal').click(function() {
+      $('.search-modal-overlay').fadeOut('fast');
+    });
+  }
+
+  function handleOutsideClick() {
+    $(document).mouseup(function(event) {
+      var container = $('.search-form');
+
+      // if the target of click is not the container div
+        // and the target is not a descendent of the container dic
+      if (!container.is(event.target) && container.has(event.target).length === 0) {
+        $('.search-modal-overlay').fadeOut('fast');
+      }
+    })
+  }
 
 });
